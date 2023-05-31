9a2514ed494552382b69ef654515f96e2dbfd204
Arthurzinho <105459168+Briefer2000@users.noreply.github.com>
2023-05-31 16:10:10 -0300

    Add files via upload

---
+++ Projeto101/dado.py
@@ -0,0 +1,50 @@
+import random
+import time
+
+response = "y"
+
+while response == "y":
+    print("Jogando o dado...")
+    time.sleep(1)
+    no = random.randint(1, 6)
+
+    if no == 1:
+        print("-----")
+        print("|   |")
+        print("| o |")
+        print("|   |")
+        print("-----")
+    elif no == 2:
+        print("-----")
+        print("|o  |")
+        print("|   |")
+        print("|  o|")
+        print("-----")
+    elif no == 3:
+        print("-----")
+        print("|o  |")
+        print("| o |")
+        print("|  o|")
+        print("-----")
+    elif no == 4:
+        print("-----")
+        print("|o o|")
+        print("|   |")
+        print("|o o|")
+        print("-----")
+    elif no == 5:
+        print("-----")
+        print("|o o|")
+        print("| o |")
+        print("|o o|")
+        print("-----")
+    else:
+        print("-----")
+        print("|o o|")
+        print("|o o|")
+        print("|o o|")
+        print("-----")
+
+    response = input("Deseja jogar o dado novamente? (y/n): ")
+
+print("Obrigado por jogar o dado!")
